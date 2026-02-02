293baae37632570cbac983ce6d74fe2be05d13a6
Ade Eka <ade.eka01@gmail.com>
2026-02-02 15:58:00 +0700

    use custom validator and new api create transfer

---
+++ utils/currency.go
@@ -0,0 +1,16 @@
+package utils
+
+const (
+	USD = "USD"
+	EUR = "EUR"
+	IDR = "IDR"
+)
+
+// IsSupportedCurrency returns true if the currency is supported
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, IDR:
+		return true
+	}
+	return false
+}
--- utils/random.go
+++ utils/random.go
@@ -42,7 +42,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"USD", "IDR"}
+	currencies := []string{USD, IDR, EUR}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
