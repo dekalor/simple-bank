3cbdf46e1dcc260ffd47e51786b5b1a6c17b0cc4
Ade Eka <ade.eka01@gmail.com>
2026-02-02 11:09:04 +0700

    temporary disable github workflows and implement env variable using viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,19 @@ import (
 	"os"
 	"testing"
 
+	"github.com/dekalor/simple-bank/utils"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig("../../")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/dekalor/simple-bank/api"
 	db "github.com/dekalor/simple-bank/db/sqlc"
+	"github.com/dekalor/simple-bank/utils"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver       = "postgres"
-	dbSource       = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverrAddress = "localhost:3000"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverrAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start the server", err)
 	}
