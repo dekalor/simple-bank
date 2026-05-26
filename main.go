2442718313b9eea5cec48f1cb3f5828d476ff2ca
Ade Eka <ade.eka01@gmail.com>
2026-05-26 11:12:18 +0700

    implement gRPC gateway

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/dekalor/simple-bank/api"
 	db "github.com/dekalor/simple-bank/db/sqlc"
 	"github.com/dekalor/simple-bank/gapi"
 	"github.com/dekalor/simple-bank/pb"
 	"github.com/dekalor/simple-bank/utils"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -27,6 +31,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 	// runGinServer(config, store)
 
@@ -51,6 +56,42 @@ func runGrpcServer(config utils.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("Cannot start gRPC server: ", err)
+	}
+}
+
+func runGatewayServer(config utils.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("Cannot create server: ", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions:   protojson.MarshalOptions{UseProtoNames: true},
+		UnmarshalOptions: protojson.UnmarshalOptions{DiscardUnknown: true},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("Cannot register gateway handler: ", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("Cannot create listener: ", err)
+	}
+
+	log.Printf("Start HTTP gateway server at %s", config.HTTPServerAddress)
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("Cannot start HTTP gateway server: ", err)
 	}
 }
 
